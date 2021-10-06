39c1fb3d532b3427da2ce1615b518190e003581d
jhj2644 <jhj2644@daum.net>
2021-10-06 12:03:44 +0900

    javascript

---
+++ 01javascript/20211006_barmemu/js/script.js
@@ -0,0 +1,36 @@
+function nowMenu(menu){ //menu = "main"
+    if(menu == "main"){
+        $(".bar").css("opacity",0)
+        console.log("메인 연결됨");
+    }
+    else{
+        $(".menu li").eq(menu).addClass("active")  //document.querySelectorAll(".menu li")[menu].classList.add("active")
+        $(".bar").css("left",200*menu)
+        console.log("서브페이지")
+    }
+
+
+
+    $(".menu li").hover(function(){
+        let menuLi = $(this).index()
+
+        if(menu == "main"){
+            $(".bar").css("opacity",1)
+        }
+
+        $(".bar").css("left",200*menuLi)
+    },function(){
+        if(menu == "main"){
+            $(".bar").css("opacity",0)
+        }
+        $(".bar").css("left",200*menu)
+    });
+
+
+
+}
+
+
+
+
+
